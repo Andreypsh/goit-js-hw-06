9f8daa30ff6c2b43f13f6217ab4307b971a02806
Andreypsh <andrey_psh@ukr.net>
2023-07-22 20:53:34 +0300

    hw06

--- js/task-06.js
+++ js/task-06.js
@@ -5,6 +5,7 @@ input.addEventListener("blur", onInputBlur);
 function onInputBlur(event) {
   const onInputValue = event.currentTarget.value.length;
   if (onInputValue === inputLength) {
+    input.classList.remove("invalid");
     input.classList.add("valid");
   } else {
     input.classList.add("valid");
